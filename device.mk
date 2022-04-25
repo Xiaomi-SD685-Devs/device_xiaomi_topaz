b199af132427c8c7d0199abe79f6f4b250389d81
Pranav Vashi <neobuddy89@gmail.com>
2022-04-26 01:14:00 +0530

    topaz: Do not build atrace HAL
    Change-Id: I71b9f8055408f6ff60e40842d488b9850358720f
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -45,10 +45,6 @@ PRODUCT_PACKAGES += \
     AntHalService-Soong \
     com.dsi.ant@1.0.vendor
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 SOONG_CONFIG_NAMESPACES += android_hardware_audio
 SOONG_CONFIG_android_hardware_audio += \
