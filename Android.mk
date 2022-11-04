e2d4c9935f6d62d3dc6a3b74cdf65d6aaad81fff
SGCMarkus <markusornik@gmail.com>
2022-11-04 17:42:56 +0100

    topaz: Add stock EGL symlinks
    Change-Id: I590b4d284fcbbe78bf860573c02287e536f090bf
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- Android.mk
+++ Android.mk
@@ -39,6 +39,22 @@ $(CNE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@rm -rf $@
 	$(hide) ln -sf /vendor/lib64/$(notdir $@) $@
 
+EGL_LIB_SYMLINKS := $(TARGET_OUT_VENDOR)/lib
+$(EGL_LIB_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL lib symlinks: $@"
+	@mkdir -p $@
+	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
+	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
+	$(hide) ln -sf egl/libq3dtools_adreno.so $@/libq3dtools_adreno.so
+
+EGL_LIB64_SYMLINKS := $(TARGET_OUT_VENDOR)/lib64
+$(EGL_LIB64_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "EGL lib64 symlinks: $@"
+	@mkdir -p $@
+	$(hide) ln -sf egl/libEGL_adreno.so $@/libEGL_adreno.so
+	$(hide) ln -sf egl/libGLESv2_adreno.so $@/libGLESv2_adreno.so
+	$(hide) ln -sf egl/libq3dtools_adreno.so $@/libq3dtools_adreno.so
+
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 IMS_SYMLINKS := $(addprefix $(TARGET_OUT_SYSTEM_EXT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
@@ -107,6 +123,6 @@ $(RFS_MSM_WPSS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	$(hide) ln -sf /vendor/firmware_mnt $@/readonly/firmware
 	$(hide) ln -sf /vendor/firmware $@/readonly/vendor/firmware
 
-ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS) $(IMS_SYMLINKS) $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_CDSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS) $(RFS_MSM_WPSS_SYMLINKS)
+ALL_DEFAULT_INSTALLED_MODULES += $(CNE_SYMLINKS) $(EGL_LIB_SYMLINKS) $(EGL_LIB64_SYMLINKS) $(IMS_SYMLINKS) $(RFS_MSM_ADSP_SYMLINKS) $(RFS_MSM_CDSP_SYMLINKS) $(RFS_MSM_MPSS_SYMLINKS) $(RFS_MSM_SLPI_SYMLINKS) $(RFS_MSM_WPSS_SYMLINKS)
 
 endif
