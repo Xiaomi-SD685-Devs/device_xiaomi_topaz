a35ddf024ae414151aba4a3dab8bbcebbf29190d
Dyneteve <dyneteve@hentaios.com>
2023-01-17 17:12:17 +0800

    topaz: overlay: Import regulatory info
    Check the device variant with ro.boot.hwc
    
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- device.mk
+++ device.mk
@@ -376,6 +376,10 @@ PRODUCT_PACKAGES += \
     FrameworksResTarget \
     FrameworksResTopaz \
     SettingsResTopaz \
+    SettingsOverlayM7G \
+    SettingsOverlayM7N \
+    SettingsOverlayM7L \
+    SettingsOverlayM7IN \
     SystemUIResCommon \
     SystemUIResTopaz \
     TelecommResCommon \
