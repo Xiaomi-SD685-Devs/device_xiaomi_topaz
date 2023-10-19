7ce6a54af5df3963d49daa8ed1ebe867f877c849
LuK1337 <priv.luk@gmail.com>
2023-10-20 00:53:15 +0200

    topaz : Change to build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -429,7 +429,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     libprotobuf-cpp-full \
-    libprotobuf-cpp-lite \
+    libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl
 
 PRODUCT_PACKAGES += \
