8068733ef9ef5bbad4e25a13fa93249d7b7d4dcf
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-10-25 14:28:37 +0700

    [DNM]topaz: Make selinux permissive
    Change-Id: I09dc0fe0e0fe6c0240c348d261ddac89bc825233

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -103,6 +103,8 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=e400000.dwc3
 
+BOARD_BOOTCONFIG += androidboot.selinux=permissive
+
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000
 
