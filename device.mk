89b7fc4ded507c102c6378ba6ef33d68f15a9bf8
boedhack99 <boedhack@gmail.com>
2023-10-27 02:28:03 +0700

    topaz: Move init scripts to default rootdir
    Change-Id: I10dee62615c2cb6dadfc988418fac50ebe399ee7

--- device.mk
+++ device.mk
@@ -289,7 +289,7 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/init/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
 # Keymaster
 PRODUCT_PACKAGES += \
