0f51998d9dec0f9e9f9eeeb3b8ba5662c0832a09
boedhack99 <boedhack@gmail.com>
2023-10-31 19:34:07 +0700

    topaz: Build source libagmmixer.so
    Change-Id: Iace17ce337030ac2d413bb1d4f495fe2b4cb0d45

--- device.mk
+++ device.mk
@@ -71,6 +71,7 @@ PRODUCT_PACKAGES += \
     libagm_compress_plugin \
     libagm_mixer_plugin \
     libagm_pcm_plugin \
+    libagmmixer \
     libbatterylistener \
     libqcompostprocbundle \
     libqcomvisualizer \
