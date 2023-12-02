814250fa4052eb72caec9162bf01c23bb1a2b399
chrisl7 <wandersonrodriguesf1@gmail.com>
2023-12-02 23:40:33 +0700

    topaz: Switch to prebuilt fingeprint service
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: I3f0ac432b47fefefcb4de2a7c7f09f641c006b47

--- device.mk
+++ device.mk
@@ -196,9 +196,6 @@ PRODUCT_PACKAGES += \
     libdrm.vendor
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
