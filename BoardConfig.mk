5d3c9ad6deedd6a158787ff576bac0d381d726aa
boedhack99 <boedhack@gmail.com>
2023-12-03 01:36:23 +0700

    topaz: Fix sepolicy denied and enforcing
    Co-authored-by: Arian <arian.kulmer@web.de>
    Co-authored-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: I5486e25a4e06d5e5ede95e9c54ac4f655290683c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,8 +104,6 @@ BOARD_BOOTCONFIG := \
     androidboot.memcg=1 \
     androidboot.usbcontroller=4e00000.dwc3
 
-BOARD_BOOTCONFIG += androidboot.selinux=permissive
-
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000
 
