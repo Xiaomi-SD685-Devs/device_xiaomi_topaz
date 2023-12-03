eebc4e0368a4e410cf26744c68d74da53d96cb44
Agatha senpai <naufalmarjan2014@gmail.com>
2023-12-03 12:11:37 +0000

    topaz: Add target common kernel header configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -110,6 +110,9 @@ BOARD_BOOTCONFIG := \
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000
 
+TARGET_KERNEL_SOURCE := kernel/msm-5.15
+TARGET_KERNEL_CONFIG := vendor/bengal_GKI.config
+
 # Kernel prebuilt
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
